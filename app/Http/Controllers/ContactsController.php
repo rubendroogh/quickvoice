d5cbd35eaeae4da47a512d1cae383badf9f99ef8
rubensPC <rubendroogh@hotmail.nl>
2018-09-17 21:22:21 +0200

    Added middleware to controllers.

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ContactsController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    
     public function listContacts(){
     	$contacts = Contact::all();
     	return view('contacts', ['contacts' => $contacts]);
--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers;
 
 use Nexmo;
+use App\Contact;
 use Illuminate\Http\Request;
 
 class SmsController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    
     public function SendSMS(Request $request){
-    	Nexmo::message()->send([
-		    'to'   => '31636572279',
-		    'from' => '16105552344',
-		    'text' => $request->message
-		]);
+    	$contacts = Contact::all();
+
+    	foreach ($contacts as $contact) {
+    		Nexmo::message()->send([
+			    'to'   => $contact->phone_number,
+			    'from' => '16105552344',
+			    'text' => $request->message
+			]);
+    	}
+    	
 
 		return redirect()->route('home');
     }
