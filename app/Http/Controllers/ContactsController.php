c7f03895e731ecbe5f5b7e85cc91f4c9343fa63e
rubensPC <rubendroogh@hotmail.nl>
2018-09-17 21:17:42 +0200

    Added contacts table.

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Contact;
 use Illuminate\Http\Request;
 
 class ContactsController extends Controller
 {
     public function listContacts(){
-    	return view('contacts');
+    	$contacts = Contact::all();
+    	return view('contacts', ['contacts' => $contacts]);
     }
 }
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -2,9 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center align-items-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
             <h1>Contacts</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <table class="table">
+				<thead>
+					<tr>
+						<th scope="col">Naam</th>
+						<th scope="col">Telefoon nummer</th>
+						<th scope="col">Opties</th>
+    				</tr>
+  				</thead>
+  				<tbody>
+  					@foreach($contacts as $contact)
+	    				<tr>
+							<th scope="row">{{ $contact->name }}</th>
+							<td>{{ $contact->phone_number }}</td>
+							<td></td>
+	    				</tr>
+    				@endforeach
+  				</tbody>
+			</table>
         </div>
     </div>
 </div>
